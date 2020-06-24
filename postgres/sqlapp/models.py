bf7e4466df303476726ed02998249787f17e0cbd
AleksanderUrbanowicz <aleksanderurbanowicz@gmail.com>
2020-06-24 16:02:10 +0200

    sql queries exercises

--- postgres/sqlapp/models.py
+++ postgres/sqlapp/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from datetime import datetime
 
+GENDER_CHOICES =[
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('U', 'Unspecified'),
+]
+
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=100)
@@ -8,11 +14,17 @@ class ProductCategory(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        db_table = "categories"
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100, null=False)
     price = models.DecimalField(decimal_places=2, max_digits=10)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "products"
 
     def __str__(self):
         return self.name
@@ -20,9 +32,17 @@ class Product(models.Model):
 
 class Client(models.Model):
     name = models.CharField(max_length=100, null=False)
+    country = models.CharField(max_length=100, blank=False)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='U')
 
     def __str__(self):
-        return self.name
+        where_from = f'{self.city}, ' if self.city is not None else ''
+        where_from += f' {self.country}'
+        return f'{self.name} from {where_from}'
+
+    class Meta:
+        db_table = "clients"
 
 
 class Transaction(models.Model):
@@ -34,3 +54,6 @@ class Transaction(models.Model):
 
     def __str__(self):
         return f' {self.code} - {self.amount} {self.product.name} for: {self.client.name}'
+
+    class Meta:
+        db_table = "transactions"
